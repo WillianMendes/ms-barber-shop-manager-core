f9a4ab340592834208c1705e5f5de7fddf0cf267
WillianMendes <willianmendesbr@gmail.com>
2022-05-24 18:34:09 -0300

    feat: config bootrstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import EnvironmentConfig from './core/config/environment/environment.config';
-import EnvironmentSchema from './core/config/environment/environment.schema';
 
 @Module({
   imports: [ConfigModule.forRoot(EnvironmentConfig.handle())],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get(ConfigService);
+  const port = config.get('PORT');
+  const host = config.get('HOST');
+
+  await app.listen(port, host, () => {
+    Logger.debug(`Application is running on: ${host}:${port}`);
+  });
 }
+
 bootstrap();
