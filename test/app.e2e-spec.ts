2fa9b3de9b6d1301c61b57c4f289e5e3338f4cfa
WillianMendes <willianmendesbr@gmail.com>
2022-06-01 18:46:16 -0300

    test: customer controller e2e template

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { plainToInstance } from 'class-transformer';
+import { CreateCustomerDto } from '../src/customer/dto/create-customer.dto';
 
 describe('CustomerController (e2e)', () => {
   let app: INestApplication;
@@ -15,7 +17,21 @@ describe('CustomerController (e2e)', () => {
     await app.init();
   });
 
-  it('/customer (POST)', () => {
-    return request(app.getHttpServer()).post('/customer').expect(201);
+  it('/customer (POST)', async () => {
+    const customerPlain = {
+      firstname: 'John',
+      lastname: 'Doe',
+      cpf: '44818464880',
+      email: 'john@email.com',
+      birthdate: '2000-01-01',
+      password: 'password123',
+    };
+
+    const customerDto = plainToInstance(CreateCustomerDto, customerPlain);
+
+    return request(app.getHttpServer())
+      .post('/customer')
+      .send(customerDto)
+      .expect(201);
   });
 });
