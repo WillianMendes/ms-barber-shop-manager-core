af63d7a1fc5e400e2883259eee29bbea37c205e0
WillianMendes <willianmendesbr@gmail.com>
2022-06-01 19:00:17 -0300

    test: should be firstname null

--- src/customer/controller/customer.controller.spec.ts
+++ src/customer/controller/customer.controller.spec.ts
@@ -18,22 +18,41 @@ describe('CustomerController', () => {
     controller = module.get<CustomerController>(CustomerController);
   });
 
-  it('should be all defined and correct', async () => {
-    const customerPlain = {
-      firstname: 'John',
-      lastname: 'Doe',
-      cpf: '44818464880',
-      email: 'john@email.com',
-      birthdate: '2000-01-01',
-      password: 'password123',
-    };
+  describe('create', () => {
+    it('should be all defined and correct', async () => {
+      const customerPlain = {
+        firstname: 'John',
+        lastname: 'Doe',
+        cpf: '44818464880',
+        email: 'john@email.com',
+        birthdate: '2000-01-01',
+        password: 'password123',
+      };
 
-    const customerDto = plainToInstance(CreateCustomerDto, customerPlain);
+      const customerDto = plainToInstance(CreateCustomerDto, customerPlain);
 
-    const result = await controller.create(customerDto);
-    const errors = await validate(customerDto);
+      const result = await controller.create(customerDto);
+      const errors = await validate(customerDto);
 
-    expect(errors.length).toBe(0);
-    expect(result).toEqual(customerDto);
+      expect(errors.length).toBe(0);
+      expect(result).toEqual(customerDto);
+    });
+
+    it('should be firstname null', async () => {
+      const customerPlain = {
+        lastname: 'Doe',
+        cpf: '44818464880',
+        email: 'john@email.com',
+        birthdate: '2000-01-01',
+        password: 'password123',
+      };
+
+      const customerDto = plainToInstance(CreateCustomerDto, customerPlain);
+
+      await controller.create(customerDto);
+      const errors = await validate(customerDto);
+
+      expect(errors.length).toBe(1);
+    });
   });
 });
