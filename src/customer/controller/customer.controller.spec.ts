045111c8f28c25a8442517f0afeefd2780fe8d15
WillianMendes <willianmendesbr@gmail.com>
2022-06-01 19:04:00 -0300

    test: should be lastname empty

--- src/customer/controller/customer.controller.spec.ts
+++ src/customer/controller/customer.controller.spec.ts
@@ -89,5 +89,23 @@ describe('CustomerController', () => {
 
       expect(errors.length).toBe(1);
     });
+
+    it('should be lastname null', async () => {
+      const customerPlain = {
+        firstname: 'John',
+        lastname: '',
+        cpf: '44818464880',
+        email: 'john@email.com',
+        birthdate: '2000-01-01',
+        password: 'password123',
+      };
+
+      const customerDto = plainToInstance(CreateCustomerDto, customerPlain);
+
+      await controller.create(customerDto);
+      const errors = await validate(customerDto);
+
+      expect(errors.length).toBe(1);
+    });
   });
 });
