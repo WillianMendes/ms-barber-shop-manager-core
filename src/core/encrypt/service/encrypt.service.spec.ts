98e912e217f81ba9a291551087438fcf15ef62bb
WillianMendes <willianmendesbr@gmail.com>
2022-06-06 13:17:25 -0300

    test: encrypt service

--- src/core/encrypt/service/encrypt.service.spec.ts
+++ src/core/encrypt/service/encrypt.service.spec.ts
@@ -1,18 +1,33 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { ModuleMetadata } from '@nestjs/common';
+import { Test, TestingModule, TestingModuleBuilder } from '@nestjs/testing';
+import spyOn = jest.spyOn;
 import { EncryptService } from './encrypt.service';
 
 describe('EncryptService', () => {
   let service: EncryptService;
 
+  const makeSpyOnServiceEncrypt = (text: string) => {
+    spyOn(service, 'encrypt').mockImplementation(() => {
+      return Promise.resolve(text);
+    });
+  };
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [EncryptService],
-    }).compile();
+    const providers = [EncryptService];
+    const exports = [EncryptService];
+    const metadata: ModuleMetadata = { providers, exports };
+    const moduleBuilder: TestingModuleBuilder =
+      Test.createTestingModule(metadata);
+    const module: TestingModule = await moduleBuilder.compile();
 
     service = module.get<EncryptService>(EncryptService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be encrypt return corrects param', async () => {
+    const passwordTest = 'password123';
+    makeSpyOnServiceEncrypt(passwordTest);
+
+    const resultHash = await service.encrypt(passwordTest);
+    expect(resultHash).toEqual(passwordTest);
   });
 });
