5ca96631e1e98078fbde79fa039fd1bced97c1a2
WillianMendes <willianmendesbr@gmail.com>
2022-06-06 13:19:23 -0300

    feat: import encrypt module

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EncryptModule } from '../../core/encrypt/encrypt.module';
 import { CustomerController } from './controller/customer.controller';
 import { CustomerService } from './service/customer.service';
 
 @Module({
-  imports: [],
+  imports: [EncryptModule],
   controllers: [CustomerController],
   providers: [CustomerService],
 })
