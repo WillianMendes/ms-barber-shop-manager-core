f19b305ecb5840674e4c0360792a73511e28d94b
WillianMendes <willianmendesbr@gmail.com>
2022-07-05 17:43:14 -0300

    chore: add start prisma client

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import VersioningOptionsCustom from './core/config/versioning/versioning.options';
 import ValidationConfig from './core/config/validation/validation.config';
 import { AppModule } from './app.module';
+import { PrismaService } from './core/config/database/service/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,9 @@ async function bootstrap() {
   const config = app.get(ConfigService);
   const port = config.get('PORT');
   const host = config.get('HOST');
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(port, host, () => {
     Logger.debug(`Application is running on: ${host}:${port}`);
